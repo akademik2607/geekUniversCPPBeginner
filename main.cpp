0c14d0b65becf08930bebe4faabeafc2778e55c5
akademik2607 <akademik2607@mail.ru>
2021-02-19 06:12:05 +0300

    Задание 6

--- main.cpp
+++ main.cpp
@@ -1,50 +1,101 @@
-#include"functions.h"
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+//1.
+bool createFile(const char* fileName, const char* text){
+    ofstream file;
+    file.open(fileName, ios::out);
+    if(!file.is_open()) return false;
+    file << text;
+    file.close();
+    return true;
+}
+
+//2.
+
+long getBuferSize(const char* fileName1, const char* fileName2){
+    ifstream file1, file2;
+    file1.open(fileName1, ios::in);
+    if(!file1.is_open()) {
+        return 0;
+    }
+
+    file2.open(fileName2);
+    if(!file2.is_open()) {
+        file1.close();
+        return 0;
+    }
+
+    long bufSize = 0;
+    char temp;
+    while(!file1.eof()){
+       file1.get(temp);
+       bufSize++;
+       cout << temp << endl;
+       cout << bufSize << endl;
+    }
+    while(!file2.eof()){
+       file2.get(temp);
+       bufSize++;
+    }
+    file1.close();
+    file2.close();
+    return bufSize;
+}
+
+bool fileConcat(const char* fileName1, const char* fileName2, char buffer[]){
+    ifstream file1, file2;
+    file1.open(fileName1);
+    if(!file1.is_open()) {
+        return false;
+    }
+
+    file2.open(fileName2);
+    if(!file2.is_open()) {
+        file1.close();
+        return false;
+    }
+    int i;
+    char temp[2];
+    for(i = 0;!file1.eof(); ++i){
+        cout << i << endl;
+       file1.get(temp, 1);
+       cout << temp;
+       buffer[i] = temp[0];
+    }
+
+    cout << i << endl;
+    for(;!file2.eof(); ++i){
+       file2.get(temp, 1);
+       buffer[i] = temp[0];
+    }
+    cout << i << endl;
+    
+    file1.close();
+    file2.close();
+    return true;
+}
+
 
 int main(){
     //1.
-    int arr1[] = {0, 1, 1, 0, 1};
-    func::first = func::printArr;
-    func::first(arr1, 5);
+    if(!createFile("file1.txt", "sdfasdfaskfj;ajfkja askdfj hello sdjfa")){
+        return 1;    
+    }
+    if(!createFile("file2.txt", "a;sdjfjaskdfjasjdfj world asjdfjlajs;lkjf)")){
+            return 1;
+    }
 
-    func::first = func::changeArr;
-    func::first(arr1, 5);
-
-    func::first = func::printArr;
-    func::first(arr1, 5);
-
-    //2.
-    int arr2[8];
-    func::second = func::fillArr;
-    func::second(arr2, 8);
-
-    func::second = func::printArr;
-    func::second(arr2, 8);
-
-    //3.
-    //[1, 1, 1, 2, 1]
-    //2, 1, 1, 2, 1
-    //[10, 1, 2, 3, 4]
-    int arr3[] = {1, 1, 1, 2, 1};
-    int arr4[] = {2, 1, 1, 2, 1};
-    int arr5[] = {10, 1, 2, 3, 4};
-    func::therd = func::checkBalance;
-
-    cout << "arr3 - " << func::therd(arr3, 5);
-    cout << "arr4 - " << func::therd(arr4, 5);
-    cout << "arr5 - " << func::therd(arr5, 5);
-
-    //4.
-    cout << endl;
-    func::forth = func::shiftArr;
-    func::printArr(arr5, 5);
-    func::forth(arr5, 5, 31);
-    func::printArr(arr5, 5);
-    func::forth(arr5, 5, -2);
-    func::printArr(arr5, 5);
-
-    //5.
-    func::fyfth = func::changeVarArgs;
-    func::fyfth(5, 0, 1, 1, 0, 1);
     
+    //2.
+    long bufSize = getBuferSize("file1.txt", "file2.txt");
+    cout << bufSize << endl;
+    char *buffer = new char[bufSize + 1];
+    fileConcat("file1.txt", "file2.txt", buffer);
+    cout << buffer << endl;
+    delete [] buffer;
+
+
     return 0;
 }
